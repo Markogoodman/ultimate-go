2d211cda197d217c769fb54f821fe26174dfc309
Markogoodman <marko958m@gmail.com>
2020-05-22 15:27:07 +0800

    Update method_2.go

--- go/language/method_2.go
+++ go/language/method_2.go
@@ -114,6 +114,8 @@ func (t Time) Add(d Duration) Time {
 // The function is using value semantics for type Time.
 // func div(t Time, d Duration) (qmod2 int, r Duration) {}
 
+
+// 自己寫的map解釋 https://hackmd.io/aDhINentQpy60lLgMTsSIg?both
 // 可以參考下列網址，unmarshal裡面會allocate記憶體，如果沒有傳指標外面會看不見，以及map是啥
 // https://stackoverflow.com/questions/45122496/why-does-json-unmarshal-need-a-pointer-to-a-map-if-a-map-is-a-reference-type/45123370
 // https://dave.cheney.net/2017/04/29/there-is-no-pass-by-reference-in-go

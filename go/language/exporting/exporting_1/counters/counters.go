1c7b2355174788cf7f9f57c84600273a388afe32
Markogoodman <marko958m@gmail.com>
2020-06-05 17:43:47 +0800

    Update counters.go

--- go/language/exporting/exporting_1/counters/counters.go
+++ go/language/exporting/exporting_1/counters/counters.go
@@ -1,6 +1,8 @@
 // Package counters provides alert counter support.
 package counters
 
+// 總結：大寫開頭可以被其他package使用，小寫不能
+
 // AlertCounter is an exported named type that contains an integer counter for alerts.
 // The first character is in upper-case format so it is considered to be exported.
 type AlertCounter int
